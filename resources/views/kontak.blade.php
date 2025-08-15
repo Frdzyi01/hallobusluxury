2d4cc17302965fe495111308aa50dfb061d0598d
Frdzyi01 <riyanfirdzi@gmail.com>
2025-08-15 14:40:15 +0700

    done minus partner

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -6,4 +6,116 @@
 @section('Title-Sub', 'Hubungi Kami')
 @section('content')
 
+    <!-- start wpo-contact-pg-section -->
+    <section class="wpo-contact-pg-section section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col col-lg-10 offset-lg-1">
+                    <div class="office-info">
+                        <div class="row">
+                            <div class="col col-xl-4 col-lg-6 col-md-6 col-12">
+                                <div class="office-info-item">
+                                    <div class="office-info-icon">
+                                        <div class="icon">
+                                            <i class="fi flaticon-placeholder"></i>
+                                        </div>
+                                    </div>
+                                    <div class="office-info-text">
+                                        <h2>Address</h2>
+                                        <p>Jl. Akses Tol Cimanggis Blk. H No.73, RT.002/RW.010, Leuwinanggung, Kec. Tapos,
+                                            Kota Depok, Jawa Barat 16456</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col col-xl-4 col-lg-6 col-md-6 col-12">
+                                <div class="office-info-item">
+                                    <div class="office-info-icon">
+                                        <div class="icon">
+                                            <i class="fi flaticon-email"></i>
+                                        </div>
+                                    </div>
+                                    <div class="office-info-text">
+                                        <h2>Email Us</h2>
+                                        <p>putratidartrans@gmail.com</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col col-xl-4 col-lg-6 col-md-6 col-12">
+                                <div class="office-info-item">
+                                    <div class="office-info-icon">
+                                        <div class="icon">
+                                            <i class="fi flaticon-phone-call"></i>
+                                        </div>
+                                    </div>
+                                    <div class="office-info-text">
+                                        <h2>Call Now</h2>
+                                        <p>+62 812-9015-3576</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="wpo-contact-title">
+                        <h2>Have Any Question?</h2>
+                        <p>It is a long established fact that a reader will be distracted
+                            content of a page when looking.</p>
+                    </div>
+                    <div class="wpo-contact-form-area">
+                        <form method="post" class="contact-validation-active" id="contact-form-main">
+                            <div>
+                                <input type="text" class="form-control" name="name" id="name"
+                                    placeholder="Your Name*">
+                            </div>
+                            <div>
+                                <input type="email" class="form-control" name="email" id="email"
+                                    placeholder="Your Email*">
+                            </div>
+                            <div>
+                                <input type="text" class="form-control" name="adress" id="adress"
+                                    placeholder="Adress">
+                            </div>
+                            <div>
+                                <select name="service" class="form-control">
+                                    <option disabled="disabled" selected="">Services</option>
+                                    <option>Architecture</option>
+                                    <option>The Rehearsal Dinner</option>
+                                    <option>The Afterparty</option>
+                                    <option>Videographers</option>
+                                    <option>Perfect Cake</option>
+                                    <option>All Of The Above</option>
+                                </select>
+                            </div>
+                            <div class="fullwidth">
+                                <textarea class="form-control" name="note" id="note" placeholder="Message..."></textarea>
+                            </div>
+                            <div class="submit-area">
+                                <button type="submit" class="theme-btn">Get in Touch</button>
+                                <div id="loader">
+                                    <i class="ti-reload"></i>
+                                </div>
+                            </div>
+                            <div class="clearfix error-handling-messages">
+                                <div id="success">Thank you</div>
+                                <div id="error"> Error occurred while sending email. Please try again later. </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- end container -->
+    </section>
+    <!-- end wpo-contact-pg-section -->
+
+    <!--  start wpo-contact-map -->
+    <section class="wpo-contact-map-section">
+        <h2 class="hidden">Contact map</h2>
+        <div class="wpo-contact-map">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.929231607518!2d106.91270847590786!3d-6.403119293587602!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ed6250bfcf57%3A0x3fe496ff3c77daf9!2sBus%20Putra%20Tidar!5e0!3m2!1sid!2sid!4v1755241782706!5m2!1sid!2sid"
+                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </section>
+    <!-- end wpo-contact-map -->
+
 @endsection
